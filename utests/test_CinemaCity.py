7fbf16d4dbb8aa7e1b4baf857df02a8205b9ba74
Jan Wadolowski <janwadolowski@gmail.com>
2021-10-07 01:24:21 +0200

    Backup json modified during utests

--- utests/test_CinemaCity.py
+++ utests/test_CinemaCity.py
@@ -1,29 +1,31 @@
 import json
 import os
-from os.path import exists
 
 import pytest
 
-import repertoire_parser.CinemaCity as cc
+from repertoire_parser.CinemaCity import _download_cinemas_list, _update_cinemas_list
 
 test_json = 'Resources/cinemas-list-test.json'
 test_json_downloaded = 'Resources/cinemas-list-test-downloaded.json'
 
+test_json_backup = None
+
 
 @pytest.fixture
 def setup():
-    pass
+    with open(file=test_json, mode='r') as f:
+        global test_json_backup
+        test_json_backup = json.load(f)
 
 
 def test_download_cinemas_list():
-    cinemas = cc._download_cinemas_list()
+    cinemas = _download_cinemas_list()
     assert type(cinemas) is dict
     assert len(cinemas.items()) > 0
 
 
 def test_update_cinemas_list():
-    cc._update_cinemas_list(test_json_downloaded)
-    assert exists(test_json)
+    _update_cinemas_list(updated_cinemas={}, path=test_json_downloaded)
     with open(test_json, 'r') as f:
         cinema_city_list = json.load(fp=f).get('cinema-city')
     assert cinema_city_list  # fails if no 'cinema-city' key in the json
@@ -35,4 +37,7 @@ def test_get_repertoire():
 
 @pytest.fixture
 def teardown():
+    with open(file=test_json, mode='w') as f:
+        json.dump(obj=test_json_backup, fp=f)
+
     os.remove(test_json_downloaded)
