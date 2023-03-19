f9971dff6d45519fda7e7025c7349bfdcab13f65
Jan Wadolowski <janwadolowski@gmail.com>
2023-03-19 06:41:35 +0100

    feat: database manager and its unit tests

---
+++ src/database/database_manager.py
@@ -0,0 +1,81 @@
+"""Module containing SQLite connection and operations wrappers.
+
+In general, functions in this class will call the database directly.
+"""
+import datetime
+from pathlib import Path
+from sqlite3 import Error
+
+import sqlalchemy
+from loguru import logger
+
+import settings
+from database.models import CinemaVenues, Repertoire
+from enums import Cinema
+from exceptions import DBConnectionError
+
+
+class DatabaseManager:
+    """Class responsible for connecting to the database and executing queries."""
+
+    def __init__(self, db_file_path: Path | str = settings.DB_FILE_PATH) -> None:
+        try:
+            engine = sqlalchemy.create_engine(f"sqlite:///{db_file_path}")
+            self._session_constructor = sqlalchemy.orm.sessionmaker(engine)
+            logger.info("Connection to the database successful.")
+        except Error as e:
+            logger.error("Unable to connect with the database: %s.", e)
+            raise DBConnectionError("Failed to connect with the database.")
+
+    def get_cinema_venues(self, cinema: Cinema, city: str | None = None) -> list[str]:
+        """Get all cinema venues for a specified cinema chain from the database.
+
+        Optionally filter by city.
+        """
+        queries = [CinemaVenues.cinema_chain == str(cinema)]
+        if city:
+            queries.append(CinemaVenues.city == city)
+
+        with self._session_constructor() as session:
+            results = (
+                session.query(CinemaVenues.venue_name)
+                .filter(*queries)
+                .group_by(CinemaVenues.city)
+                .all()
+            )
+            return [item[0] for item in results]
+
+    def update_cinema_venues(self, venues: list[CinemaVenues]) -> None:
+        """Update cinema venues in the database.
+
+        Function will remove all records from the table and insert new ones.
+        """
+        with self._session_constructor() as session:
+            session.query(CinemaVenues).delete()
+            session.add_all(venues)
+
+    def get_repertoire(
+            self,
+            date: datetime.date,
+            cinema: Cinema,
+            *,
+            venue: str | None = None,
+            city: str | None = None,
+            format: str | None = None,
+            language: str | None = None,
+    ) -> list[Repertoire]:
+        """Get repertoire matching specified date and cinema."""
+        with self._session_constructor() as session:
+            clauses = [
+                Repertoire.play_time == date,
+                CinemaVenues.cinema_chain == str(cinema),
+            ]
+            if venue:
+                clauses.append(CinemaVenues.venue_name == venue)
+            if city:
+                clauses.append(CinemaVenues.city == city)
+            if format:
+                clauses.append(Repertoire.movie_format == format)
+            if language:
+                clauses.append(Repertoire.movie_language == language)
+            return session.query(Repertoire).filter(*clauses).all()
---
+++ tests/unittests/database/test_database_manager.py
@@ -0,0 +1,129 @@
+import datetime
+from sqlite3 import Error
+
+import pytest
+import sqlalchemy
+from mockito import mock, when, args
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.query import RowReturningQuery, Query
+from tomlkit import datetime
+
+from database.database_manager import DatabaseManager
+from database.models import CinemaVenues
+from enums import Cinema
+from exceptions import DBConnectionError
+
+
+@pytest.fixture
+def db_manager() -> DatabaseManager:
+    return DatabaseManager()
+
+
+@pytest.fixture
+def engine() -> sqlalchemy.Engine:
+    return mock(sqlalchemy.Engine)
+
+
+@pytest.fixture
+def session() -> Session:
+    return mock(Session)
+
+
+@pytest.fixture
+def query() -> Query:
+    return mock(Query)
+
+
+@pytest.fixture
+def row_returning_query() -> RowReturningQuery:
+    return mock(RowReturningQuery)
+
+
+def test_get_cinema_venues_returns_venues_without_city_filter(
+        db_manager: DatabaseManager,
+        session: Session,
+        row_returning_query: RowReturningQuery,
+) -> None:
+    expected = ["Cinema 1", "Cinema 2"]
+
+    when(session).__enter__().thenReturn(session)
+    when(session).__exit__(*args)
+    when(db_manager)._session_constructor().thenReturn(session)
+    when(session).query(CinemaVenues.venue_name).thenReturn(row_returning_query)
+    when(row_returning_query).filter(...).thenReturn(row_returning_query)
+    when(row_returning_query).group_by(CinemaVenues.city).thenReturn(
+        row_returning_query
+    )
+    when(row_returning_query).all().thenReturn([("Cinema 1",), ("Cinema 2",)])
+
+    assert db_manager.get_cinema_venues(Cinema.CINEMA_CITY) == expected
+
+
+def test_get_cinema_venues_returns_venues_with_city_filter(
+        db_manager: DatabaseManager,
+        session: Session,
+        row_returning_query: RowReturningQuery,
+) -> None:
+    expected = ["Cinema 1", "Cinema 2"]
+
+    when(session).__enter__().thenReturn(session)
+    when(session).__exit__(*args)
+    when(db_manager)._session_constructor().thenReturn(session)
+    when(session).query(CinemaVenues.venue_name).thenReturn(row_returning_query)
+    when(row_returning_query).filter(...).thenReturn(row_returning_query)
+    when(row_returning_query).group_by(CinemaVenues.city).thenReturn(
+        row_returning_query
+    )
+    when(row_returning_query).all().thenReturn([("Cinema 1",), ("Cinema 2",)])
+
+    assert db_manager.get_cinema_venues(Cinema.CINEMA_CITY, "some city") == expected
+
+
+def test_update_cinema_venues_inserts_records_to_db(
+        db_manager: DatabaseManager, session: Session, query: Query
+) -> None:
+    venues = ["Cinema Venue 1", "Cinema Venue 1"]
+
+    when(session).__enter__().thenReturn(session)
+    when(session).__exit__(*args)
+    when(db_manager)._session_constructor().thenReturn(session)
+    when(session).query(CinemaVenues).thenReturn(query)
+    when(query).delete()
+    when(session).add_all(venues)
+
+    db_manager.update_cinema_venues(venues)
+
+
+def test_get_repertoire_returns_repertoire(
+        db_manager: DatabaseManager,
+        session: Session,
+        row_returning_query: RowReturningQuery,
+) -> None:
+    expected = ["Movie 1", "Movie 2"]
+
+    when(session).__enter__().thenReturn(session)
+    when(session).__exit__(*args)
+    when(db_manager)._session_constructor().thenReturn(session)
+    when(session).query(...).thenReturn(row_returning_query)
+    when(row_returning_query).filter(...).thenReturn(row_returning_query)
+    when(row_returning_query).all().thenReturn(
+        [("Repertoire Entry 1",), ("Repertoire Entry  2",)]
+    )
+
+    db_manager.get_repertoire(
+        datetime.date(2022, 1, 10),
+        Cinema.CINEMA_CITY,
+        venue="some venue",
+        city="some city",
+        format="some format",
+        language="some language",
+    )
+
+
+def test_database_manager_fails_to_create_instance_due_to_error() -> None:
+    when(sqlalchemy).create_engine("sqlite:///some path").thenRaise(
+        Error("some connection error")
+    )
+
+    with pytest.raises(DBConnectionError, match="Failed to connect with the database."):
+        DatabaseManager("some path")
