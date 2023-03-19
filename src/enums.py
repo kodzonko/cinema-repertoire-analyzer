953a385873e78b38e14f58baab9d06d6eacd709e
Jan Wadolowski <janwadolowski@gmail.com>
2023-03-19 06:42:01 +0100

    feat: enum and custom exceptions

---
+++ src/enums.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+
+class Cinema(StrEnum):
+    CINEMA_CITY = "Cinema City"
+    HELIOS = "Helios"
+    MULTIKINO = "Multikino"
---
+++ src/exceptions.py
@@ -0,0 +1,6 @@
+class SettingsLoadError(Exception):
+    """Raised when the settings file cannot be loaded."""
+
+
+class DBConnectionError(Exception):
+    """Raised when the database connection cannot be established."""
