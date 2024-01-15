1880e2535612ee105ba2f4d28f4ff83479a4c9ee
Jan Wadolowski <janwadolowski@gmail.com>
2024-01-15 19:31:10 +0100

    feat: add protocol for cinemas

---
+++ src/cinema_repertoire_analyzer/cinema_api/cinema.py
@@ -0,0 +1,28 @@
+"""Module containing logic interacting with cinema websites.
+
+Due to lack of official APIs for the supported cinemas, data is downloaded using web
+scraping. This module is the de-facto simplified API for the supported cinemas.
+
+Be mindful of the fact that the websites may change their structure at any time,
+urls have been abstracted away to the config file, but the logic may need to be affected
+as well.
+
+Don't overuse these functions, as too many requests may result in a ban from the
+website.
+"""
+
+from abc import abstractmethod
+from datetime import datetime
+from typing import Protocol
+
+from cinema_repertoire_analyzer.cinema_api.models import CinemaVenue, Repertoire
+
+
+class Cinema(Protocol):
+    @abstractmethod
+    def fetch_repertoire(self, date: datetime, cinema_venue: str) -> list[Repertoire]:
+        """Download repertoire for a specified date from the cinema website."""
+
+    @abstractmethod
+    def fetch_cinema_venues_list(self) -> list[CinemaVenue]:
+        """Download list of cinema venues from the cinema website."""
