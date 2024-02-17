a6d65a33321fba497515721e276ace66829d8b00
Jan Wadolowski <janwadolowski@gmail.com>
2024-02-17 03:13:36 +0100

    fix: move fill_string_template to separate module
    to avoid circular imports issue

---
+++ src/cinema_repertoire_analyzer/cinema_api/template_utils.py
@@ -0,0 +1,25 @@
+from cinema_repertoire_analyzer.exceptions import SettingsLoadError
+
+
+def fill_string_template(text: str, **kwargs) -> str:
+    """
+    Verify that all variables in string are matched in the variables dictionary.
+
+    Args:
+        text: A string to parse.
+        kwargs: Variables to format the string.
+
+    Returns:
+        True if all variables are present, False otherwise.
+
+    Raises:
+        SettingsLoadError: If some variables are missing.
+    """
+    try:
+        return text.format(**kwargs)
+    except IndexError:  # means no placeholders to substitute
+        return text
+    except KeyError as e:  # means some variables are missing
+        raise SettingsLoadError(
+            "Unable to fill url template to make a request. Missing variable: %s." % e
+        )
