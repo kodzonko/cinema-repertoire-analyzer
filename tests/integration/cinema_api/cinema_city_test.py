86ba4dfe5f71c2317aee5a8613f52abc1cfff2cd
Jan Wadolowski <janwadolowski@gmail.com>
2024-05-02 02:14:43 +0100

    chore: update ruff config

--- tests/integration/cinema_api/cinema_city_test.py
+++ tests/integration/cinema_api/cinema_city_test.py
@@ -27,4 +27,5 @@ def test_fetch_repertoire_downloads_and_parses_cinema_city_repertoire_correctly(
 
 
 @pytest.mark.integration
-def test_fetch_cinema_venues_list_downloads_list_of_cinema_venues_correctly() -> None: ...
+def test_fetch_cinema_venues_list_downloads_list_of_cinema_venues_correctly() -> None:
+    ...
