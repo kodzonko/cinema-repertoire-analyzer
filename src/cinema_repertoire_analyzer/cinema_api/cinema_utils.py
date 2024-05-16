c400740f98ddc4cd9ac023ab745a49cb7f62721c
Jan Wadolowski <janwadolowski@gmail.com>
2024-05-16 21:24:06 +0200

    feat: renaming settings vars

--- src/cinema_repertoire_analyzer/cinema_api/cinema_utils.py
+++ src/cinema_repertoire_analyzer/cinema_api/cinema_utils.py
@@ -2,11 +2,12 @@ from cinema_repertoire_analyzer.cinema_api.cinema import Cinema
 from cinema_repertoire_analyzer.cinema_api.cinema_city import CinemaCity
 from cinema_repertoire_analyzer.cinema_api.helios import Helios
 from cinema_repertoire_analyzer.cinema_api.multikino import Multikino
+from cinema_repertoire_analyzer.database.models import VenueData
 from cinema_repertoire_analyzer.enums import CinemaChain
 from cinema_repertoire_analyzer.settings import CinemaSettings, Settings
 
 
-def get_cinema_class_by_cinema_chain(cinema_chain: CinemaChain) -> type[Cinema]:
+def _get_cinema_class_by_cinema_chain(cinema_chain: CinemaChain) -> type[Cinema]:
     """Get the cinema class for the given cinema chain."""
     cinema_name_to_cinema_class_mapping = {
         CinemaChain.CINEMA_CITY: CinemaCity,
@@ -16,24 +17,35 @@ def get_cinema_class_by_cinema_chain(cinema_chain: CinemaChain) -> type[Cinema]:
     return cinema_name_to_cinema_class_mapping[cinema_chain]
 
 
-def get_cinema_settings_by_cinema_chain(
+def _get_cinema_settings_by_cinema_chain(
     cinema_chain: CinemaChain, settings: Settings
 ) -> CinemaSettings:
     """Get the cinema class for the given cinema chain."""
     cinema_class_to_config_mapping = {
-        CinemaChain.CINEMA_CITY: settings.cinema_city_settings,
-        CinemaChain.HELIOS: settings.helios_settings,
-        CinemaChain.MULTIKINO: settings.multikino_settings,
+        CinemaChain.CINEMA_CITY: settings.CINEMA_CITY_SETTINGS,
+        CinemaChain.HELIOS: settings.HELIOS_SETTINGS,
+        CinemaChain.MULTIKINO: settings.MULTIKINO_SETTINGS,
     }
     return cinema_class_to_config_mapping[cinema_chain]
 
 
+def _get_venue_data_for_specific_cinema_chain(
+    cinema_chain: CinemaChain, venue_data: VenueData
+) -> str:
+    """Depending on the cinema chain, return either venue id or venue name needed for the
+    repertoire fetcher.
+    """  # noqa: D205
+    if cinema_chain == CinemaChain.MULTIKINO:
+        return venue_data.venue_name
+    return venue_data.venue_id
+
+
 def cinema_factory(cinema_chain: CinemaChain, settings: Settings) -> Cinema:
     """Factory function for creating cinema objects."""
-    cinema_class: type[Cinema] = get_cinema_class_by_cinema_chain(cinema_chain)
-    cinema_settings = get_cinema_settings_by_cinema_chain(cinema_chain, settings)
+    cinema_class: type[Cinema] = _get_cinema_class_by_cinema_chain(cinema_chain)
+    cinema_settings = _get_cinema_settings_by_cinema_chain(cinema_chain, settings)
 
     return cinema_class(
-        repertoire_url=cinema_settings.repertoire_url,
-        cinema_venues_url=cinema_settings.venues_list_url,
+        repertoire_url=cinema_settings.REPERTOIRE_URL,
+        cinema_venues_url=cinema_settings.VENUES_LIST_URL,
     )
