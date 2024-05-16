ee91e01a20457b10c94231829a9cc21bd38cc666
Jan Wadolowski <janwadolowski@gmail.com>
2024-05-16 21:18:10 +0200

    feat: update url filling template

--- src/cinema_repertoire_analyzer/cinema_api/template_utils.py
+++ src/cinema_repertoire_analyzer/cinema_api/template_utils.py
@@ -1,7 +1,8 @@
-from cinema_repertoire_analyzer.exceptions import SettingsLoadError
+import typer
+from pydantic_core import Url
 
 
-def fill_string_template(text: str, **kwargs) -> str:
+def fill_string_template(text: str | Url, **kwargs) -> str:
     """Verify that all variables in string are matched in the variables dictionary.
 
     Args:
@@ -15,10 +16,9 @@ def fill_string_template(text: str, **kwargs) -> str:
         SettingsLoadError: If some variables are missing.
     """
     try:
-        return text.format(**kwargs)
+        return str(text).format(**kwargs)
     except IndexError:  # means no placeholders to substitute
-        return text
+        return str(text)
     except KeyError as e:  # means some variables are missing
-        raise SettingsLoadError(
-            f"Unable to fill url template to make a request. Missing variable: {e}."
-        )
+        typer.echo(f"Nie udało się wypełnić templatki z adresem url. Brakująca zmienna: {e}.")
+        raise typer.Exit(code=1)
